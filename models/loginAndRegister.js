19c1a3111381b7d14ef04cfdd702d26ff53b1900
Shay Avraham <shay@picsize.co.il>
2014-10-12 12:19:35 +0300

    register 1.2

--- models/loginAndRegister.js
+++ models/loginAndRegister.js
@@ -63,12 +63,14 @@ $(function () {
 
     //display cover image
     $('#coverImgFile').change(function () {
-        showImgPreview(this, 'cover');
+        //showImgPreview(this, 'cover');
+        showImgPreview('cover');
     });
 
     //display profile image
     $('#profileImgFile').change(function () {
-        showImgPreview(this, 'profile');
+        //showImgPreview(this, 'profile');
+        showImgPreview('profile');
     });
 });
 
@@ -174,11 +176,40 @@ function showImgPreview(input, type) {
             else {
                 $('#uploadProfileImg').css('background-image', 'url(' + e.target.result + ')');
             }
-            
+
         }
 
         reader.readAsDataURL(input.files[0]);
     }
+}
+
+function showImgPreview(type) {
+    if (type === 'cover') {
+        navigator.camera.getPicture(showCover, onFail, {
+            quality: 50,
+            destinationType: Camera.DestinationType.FILE_URI,
+            sourceType: Camera.PictureSourceType.PHOTOLIBRARY
+        });
+    }
+    else {
+        navigator.camera.getPicture(showProfile, onFail, {
+            quality: 50,
+            destinationType: Camera.DestinationType.FILE_URI,
+            sourceType: Camera.PictureSourceType.PHOTOLIBRARY
+        });
+    }
+}
+
+function showCover(imageURI) {
+    $('#uploadCoverImg').attr('src', imageURI)
+}
+
+function showProfile(imageURI) {
+    $('#uploadProfileImg').css('background-image', 'url(' + imageURI + ')');
+}
+
+function onFail(message) {
+    alert('Failed because: ' + message);
 }
 
 //#endregion
