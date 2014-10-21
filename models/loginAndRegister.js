8bb0538666046d80d64721edbfe2330c84993dbe
Shay Avraham <shay@picsize.co.il>
2014-10-21 13:12:56 +0300

    fix 3.4

--- models/loginAndRegister.js
+++ models/loginAndRegister.js
@@ -52,11 +52,13 @@ $(function () {
 
     //upload cover img
     $(document).on('click', '#uploadCoverImg', function () {
+        alert('click on cover image');
         $('#coverImgFile').click();
     })
 
     //upload profile img
     $(document).on('click', '#uploadProfileImg', function () {
+        alert('click on profile image');
         $('#profileImgFile').click();
     })
 
@@ -157,6 +159,39 @@ FB.Event.subscribe('auth.login', function (response) {
 
 //#endregion
 
+//#region Register Form
+
+function showImgPreview(type) {
+    alert('showImgPreview function');
+    if (type === 'cover') {
+        navigator.camera.getPicture(showCover, onFail, {
+            quality: 50,
+            destinationType: Camera.DestinationType.FILE_URI,
+            sourceType: Camera.PictureSourceType.PHOTOLIBRARY
+        });
+    }
+    else {
+        navigator.camera.getPicture(showProfile, onFail, {
+            quality: 50,
+            destinationType: Camera.DestinationType.FILE_URI,
+            sourceType: Camera.PictureSourceType.PHOTOLIBRARY
+        });
+    }
+}
+
+function showCover(imageURI) {
+    $('#uploadCoverImg').attr('src', imageURI)
+}
+
+function showProfile(imageURI) {
+    $('#uploadProfileImg').css('background-image', 'url(' + imageURI + ')');
+}
+
+function onFail(message) {
+    alert('Failed because: ' + message);
+}
+
+//#endregion
 
 
 
--- models/uploadImages.js
+++ models/uploadImages.js
@@ -5,39 +5,7 @@
 */
 $(function () {
 
-    //#region Register Form
-
-    function showImgPreview(type) {
-        alert('showImgPreview function');
-        if (type === 'cover') {
-            navigator.camera.getPicture(showCover, onFail, {
-                quality: 50,
-                destinationType: Camera.DestinationType.FILE_URI,
-                sourceType: Camera.PictureSourceType.PHOTOLIBRARY
-            });
-        }
-        else {
-            navigator.camera.getPicture(showProfile, onFail, {
-                quality: 50,
-                destinationType: Camera.DestinationType.FILE_URI,
-                sourceType: Camera.PictureSourceType.PHOTOLIBRARY
-            });
-        }
-    }
-
-    function showCover(imageURI) {
-        $('.uploadCoverImg').attr('src', imageURI)
-    }
-
-    function showProfile(imageURI) {
-        $('.uploadProfileImg').css('background-image', 'url(' + imageURI + ')');
-    }
-
-    function onFail(message) {
-        alert('Failed because: ' + message);
-    }
-
-    //#endregion
+ 
 
 });
 
