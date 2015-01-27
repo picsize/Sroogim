bf38d6ef83a69ee12582c79dd902f3d6fa1cfcac
Shay Avraham <shay@picsize.co.il>
2015-01-27 17:13:53 +0200

    f5.1

--- models/app.js
+++ models/app.js
@@ -259,22 +259,20 @@ function initFacebook() {
             appId: "988309234528102",
             nativeInterface: CDV.FB,
             useCachedDialogs: false,
-            oauth: true,
-            status: true,                                 // Check Facebook Login status
-            xfbml: true                                  // Look for social plugins on the page
+            oauth: true
         });
         getLoginStatus();
         // Additional initialization code such as adding Event Listeners goes here
     };
 
-    // Load the SDK asynchronously
-    (function (d, s, id) {
-        var js, fjs = d.getElementsByTagName(s)[0];
-        if (d.getElementById(id)) { return; }
-        js = d.createElement(s); js.id = id;
-        js.src = "//connect.facebook.net/en_US/all.js";
-        fjs.parentNode.insertBefore(js, fjs);
-    }(document, 'script', 'facebook-jssdk'));
+    //// Load the SDK asynchronously
+    //(function (d, s, id) {
+    //    var js, fjs = d.getElementsByTagName(s)[0];
+    //    if (d.getElementById(id)) { return; }
+    //    js = d.createElement(s); js.id = id;
+    //    js.src = "//connect.facebook.net/en_US/all.js";
+    //    fjs.parentNode.insertBefore(js, fjs);
+    //}(document, 'script', 'facebook-jssdk'));
 
 }
 
