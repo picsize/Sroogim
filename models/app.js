bbfad54ede53c4eee7d8164ce728c6dc6079d5dd
Shay Avraham <shay@picsize.co.il>
2015-02-02 21:18:43 +0200

    share 2.1

--- models/app.js
+++ models/app.js
@@ -14,6 +14,24 @@ document.addEventListener("deviceready", initApp, false);
 //});
 
 function initApp() {
+    $('#menuSidebar').panel().enhanceWithin();
+    $('#popup').enhanceWithin().popup();
+    $('#newsContainer p').marquee();
+    checkPhonegap();
+    getAllDates();
+    getAllPresents();
+    getAllCategories();
+    getAllLocations();
+    initFacebook();
+    getcurrentlatlong();
+
+    var devicePlatform = device.platform;
+    if (devicePlatform.toLowerCase().indexOf('ios') != -1) {
+        if (navigator.userAgent.match(/(iPad.*|iPhone.*|iPod.*);.*CPU.*OS 7_\d/i)) {
+            StatusBar.hide();
+        }
+    }
+
     var count = 7;
     var loadingText = {
         '6': 'טוען רכיבים',
@@ -44,24 +62,6 @@ function initApp() {
         }
     }
     loadComponents();
-
-    $('#menuSidebar').panel().enhanceWithin();
-    $('#popup').enhanceWithin().popup();
-    $('#newsContainer p').marquee();
-    checkPhonegap();
-    getAllDates();
-    getAllPresents();
-    getAllCategories();
-    getAllLocations();
-    initFacebook();
-    getcurrentlatlong();
-
-    var devicePlatform = device.platform;
-    if (devicePlatform.toLowerCase().indexOf('ios') != -1) {
-        if (navigator.userAgent.match(/(iPad.*|iPhone.*|iPod.*);.*CPU.*OS 7_\d/i)) {
-            StatusBar.hide();
-        }
-    }
 
 }
 
